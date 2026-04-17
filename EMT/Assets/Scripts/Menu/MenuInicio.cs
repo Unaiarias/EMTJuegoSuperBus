8ea3b97fa3be692d0ebe12152afeef2f863077df
SrTibii <marcluengoferra@gmail.com>
2026-04-17 21:42:07 +0200

    Sonido click en los botones

--- EMT/Assets/Scripts/Menu/MenuInicio.cs
+++ EMT/Assets/Scripts/Menu/MenuInicio.cs
@@ -3,16 +3,30 @@ using UnityEngine.SceneManagement;
 
 public class MenuInicio : MonoBehaviour
 {
+    private AudioSource audioSource;
+    public AudioClip sonidoBotonPresionado;
+    public AudioClip sonidoBotonSeleccionado;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void BotonClickAudio()
+    {
+        audioSource.PlayOneShot(sonidoBotonPresionado);
+    }
+
+    public void BotonSeleccionadoAudio()
+    {
+        audioSource.PlayOneShot(sonidoBotonSeleccionado);
     }
 
     public void NivelXativa() 
